c38d579170b9dfcf20141598b758156f7bac26e3
lahari <tnlahari@gmail.com>
2021-05-20 17:37:42 -0500

    passport

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
+var passport = require('passport');
+var authenticate = require('./routes/auth.js');
 
 const mongoose = require('mongoose');
 
@@ -34,7 +36,6 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 //app.use(cookieParser('12345-67890-09876-54321'));
-
 app.use(session({
   name: 'session-id',
   secret: '12345-67890-09876-54321',
@@ -42,33 +43,27 @@ app.use(session({
   resave: false,
   store: new FileStore()
 }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 function auth (req, res, next) {
-  console.log(req.session);
+  console.log(req.user);
 
-  if (!req.session.user) {
+  if (!req.user) {
       var err = new Error('You are not authenticated!');
       err.status = 403;
-      return next(err);
+      next(err);
   }
   else{
-    if(req.session.user === 'authenticated') {
       next();
     }
-    else {
-      var err = new Error('You are not authenticated!');
-      err.status = 403;
-      return next(err);
-    }
-  }
 }
   
 app.use(auth);
 app.use(express.static(path.join(__dirname, 'public')));
-
 
 app.use('/dishes',dishRouter);
 app.use('/promotions',promotionRouter);
